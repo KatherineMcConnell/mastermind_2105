5b5cb4fbbf5a6df6498bc00a778476af5e46420e
Marla Schulz <marlaa.schulz@gmail.com>
2021-05-18 21:11:03 -0600

    Add input message methods and logic

--- lib/game.rb
+++ lib/game.rb
@@ -1,29 +1,52 @@
 class Game
   def initialize
-
+    @code_breaker_input #??
   end
 
   def start
       p message.welcome
-      code_breaker_input = gets.chomp
   end
 
   def start_menu
-    if code_breaker_input == 'p' || code_breaker_input == 'play'
-      self.beginning_guess
-    elsif code_breaker_input == 'i' || 'instructions'
-      p message.instructions
-      self.start
-    elsif code_breaker_input == 'q' || 'quit'
+    @code_breaker_input = gets.chomp
+    self.quit
+    self.instructions
+    self.play
+    self.cheat
+
+  end
+
+  def quit
+    #double check calling class variables inside class
+    if @code_breaker_input == 'q' || 'quit'
+      p message.quit
       exit
-    else
-      p message.error
     end
   end
 
-  def beginning_guess
-    p message.beginner_sequence
-    guess = gets.chomp
+  def instructions
+    if @code_breaker_input == 'i' || 'instructions'
+      p message.instructions
+    end
+  end
+
+  def play
+    if @code_breaker_input == 'p' || 'play'
+      p message.beginner_sequence
+      # run method that grabs user guess
+    end
+  end
+
+  def cheat
+    if @code_breaker_input == 'c' || 'cheat'
+      p message.cheat
+    end
+  end
+
+  def invalid_input
+    if !@code_breaker_input == 'q' || 'quit' || 'i' || 'instructions' || 'p' || 'play' || 'c' || 'cheat'
+      p message.invalid_input
+    end
   end
 
   def game_flow
@@ -33,9 +56,8 @@ class Game
     if guess == message.correct_guess
       p message.correct_guess
       start_menu
+    end
   end
-
-
 
 
 
