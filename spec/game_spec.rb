8a78301e6b9bff6453e575953b0bcbd7159c138c
Marla Schulz <marlaa.schulz@gmail.com>
2021-05-20 19:53:32 -0600

    Add game flow loop, and tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -10,6 +10,24 @@ RSpec.describe Game do
     expect(game).to be_an_instance_of(Game)
   end
 
+  it 'has attributes' do
+    game = Game.new
+
+    expect(game.guess_count).to eq(0)
+    expect(game.player).to be_an_instance_of(Player)
+  end
+
+  it 'can start' do
+  end
+
+  it 'has a menu' do
+  end
+
+  it 'can play a game' do
+    game = Game.new
+
+  end
+
   it 'prints message & records input' do
     game = Game.new
 
