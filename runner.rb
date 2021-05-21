a83bb52580d318dc5e695ef78e8a483cddc47278
Marla Schulz <marlaa.schulz@gmail.com>
2021-05-21 16:20:30 -0600

    Update runner file with get user input loop

--- runner.rb
+++ runner.rb
@@ -1,5 +1,14 @@
 require './lib/game'
+require './lib/sequence'
+require './lib/messages'
+require './lib/compare'
 
 game = Game.new.start
 #loop for getting userinfo and sending off
-user_input = gets.chomp
+loop do
+  user_input = gets.chomp.downcase
+  game.game_flow
+  if game.compare.is_quit? == true
+    break
+  end
+end
