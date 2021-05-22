74061d90ec7d4ff923a419104da98de48b26cf16
Marla Schulz <marlaa.schulz@gmail.com>
2021-05-21 19:46:22 -0600

    Change gets chomp method to player class

--- runner.rb
+++ runner.rb
@@ -2,17 +2,18 @@ require './lib/game'
 require './lib/sequence'
 require './lib/messages'
 require './lib/compare'
+require './lib/player'
 require 'pry'
 
 message = Message.new
-game = Game.new(message)
-game.start
+game = Game.new(message, message.compare.player)
+game.play_game
 #loop for getting userinfo and sending off
-loop do
-  chomp_input = gets.chomp.downcase
-  #binding.pry
-  game.game_flow
-  if game.message.compare.is_quit? == true
-    break
-  end
-end
+# loop do
+#   chomp_input = gets.chomp.downcase
+#   #binding.pry
+#   game.game_flow
+#   if game.message.compare.is_quit? == true
+#     break
+#   end
+# end
