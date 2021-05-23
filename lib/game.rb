efd76e92528b32e2149f66fd3fc5dca50a3dc230
KatherineMcConnell <mcconnell.katherine01@gmail.com>
2021-05-23 12:22:51 -0600

    Refactor end_game method in game_class to exit! game

--- lib/game.rb
+++ lib/game.rb
@@ -60,31 +60,6 @@ class Game
     end
   end
 
-  # def game_flow
-  #   binding.pry
-  #   loop do
-  #     @player.get_user_input
-  #     if @message.compare.is_guess? == true
-  #       @compare.guess_counter
-  #       if @message.compare.too_long == true
-  #         @message.too_long
-  #       elsif  @message.compare.too_short == true
-  #         @messages.too_short
-  #       elsif @messages.compare.user_won? == false
-  #         @messages.feedback
-  #       end
-  #     elsif @message.compare.is_menu_input? == true
-  #       self.start_menu
-  #     else
-  #       @message.invalid_input
-  #     end
-  #     if @message.compare.is_quit? == true
-  #       @message.exit_message
-  #       break
-  #     end
-  #   end
-  # end
-
   def play_game
     self.start
     self.start_menu
@@ -93,6 +68,7 @@ class Game
   def end_game
     if @message.compare.is_quit? == true
        @message.exit_message
+       exit!
     end
   end
 
