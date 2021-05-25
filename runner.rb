648780f3fcde084c6ea0f17d8236d710b9e5864a
KatherineMcConnell <mcconnell.katherine01@gmail.com>
2021-05-24 21:00:48 -0600

    Committing deletions of commenting out

--- runner.rb
+++ runner.rb
@@ -10,12 +10,3 @@ compare = Compare.new(sequence.random_sequence)
 message = Message.new(compare)
 game = Game.new(message, message.compare.player)
 game.play_game
-#loop for getting userinfo and sending off
-# loop do
-#   chomp_input = gets.chomp.downcase
-#   #binding.pry
-#   game.game_flow
-#   if game.message.compare.is_quit? == true
-#     break
-#   end
-# end
